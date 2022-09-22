6f31cc4cb2471c65431a6c37b825d7dbb9fa8e12
emmanuel obarakpo <emmanuelobarakpo@gmail.com>
2022-09-22 23:52:55 +0100

    display image component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import Search from './components/search/search';
+import Image from './components/image/image';
 
 function App() {
 
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
+    console.log(searchData.value1);
   }
 
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange}/>
+      <Image />
     </div>
   );
 }
